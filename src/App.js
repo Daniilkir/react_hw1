d27c80ba4b337adf7a0d5aea3d44a3431aaba7c2
Daniil <d.kirykha2010@gmail.com>
2024-08-27 18:11:17 +0300

    hw

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
+  const myName = "Даня";
+  const welcomeText = "Welcome";
+  const imageUrl = "https://blog.ferplast.com/wp-content/uploads/2015/08/tardar-sauce-grumpy-cat-gatto-pi%C3%B9-ricco-del-mondo-ferplast-979x652.jpg";
+  const imageAlt = "Котик";
+  const favSite = { name: "Wikipedia", url: "https://uk.wikipedia.org/wiki" };
+  const number1 = 52;
+  const number2 = 27;
+  const sum = number1 + number2;
+  const colors = ["Фіолетовий", "Синій", "Жовтий"];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{myName}</h1>
+      <p>{welcomeText}</p>
+      <img src={imageUrl} alt={imageAlt} />
+      <p>Мій улюблений сайт: <a href={favSite.url}>{favSite.name}</a></p>
+      <p>Сума: {sum}</p>
+      <ul>
+        {colors.map((color, index) => (
+          <li key={index}>{color}</li>
+        ))}
+      </ul>
     </div>
   );
 }
